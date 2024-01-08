0c7c9c272eb7b75e3740fa95b061982d967abfde
KBear100 <kgamett@student.neumont.edu>
2024-01-08 15:29:07 -0700

    Basic UI

---
+++ Assets/Scripts/DialogSystem.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogSystem : MonoBehaviour
+{
+    [SerializeField] TMP_Text voiceLineText;
+    [SerializeField] Image characterPicture;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
