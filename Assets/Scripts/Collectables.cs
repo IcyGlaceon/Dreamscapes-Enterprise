22213d2480c4e7771cf85a476efbf506038dcaa7
EthanBarlevy <98473724+EthanBarlevy@users.noreply.github.com>
2024-01-08 16:30:24 -0700

    started some of the work on restart

--- Assets/Scripts/Collectables.cs
+++ Assets/Scripts/Collectables.cs
@@ -7,7 +7,7 @@ public class Collectables : MonoBehaviour
     [SerializeField] GameManager gameManager;
    
 
-    private void OnCollisionEnter2D(Collision collision)
+    private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.tag == "Player")
         {
--- Assets/Scripts/Restart.cs
+++ Assets/Scripts/Restart.cs
@@ -1,13 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Restart : MonoBehaviour
 {
     [SerializeField] GameObject RestartUI;
 
     public void RestartButton()
-    { 
-        
+    {
+        SceneManager.LoadScene("StartScreen");
     }
 }
