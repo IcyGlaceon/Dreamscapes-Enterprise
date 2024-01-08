d32b7b7fee6a62ba37ea4db15414b930120e65d6
Carter Small <melaniesmall461@gmail.com>
2024-01-08 16:27:47 -0700

    AHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH

--- Assets/Scripts/StartScreen.cs
+++ Assets/Scripts/StartScreen.cs
@@ -6,18 +6,6 @@ using UnityEngine.SceneManagement;
 
 public class StartScreen : MonoBehaviour
 {
-
-    // Start is called before the first frame update
-    void Start()
-    {
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     public void CharacterSelection()
     {
         SceneManager.LoadScene("CharacterSelection");
