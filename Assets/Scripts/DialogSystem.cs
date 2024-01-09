17e113c03ffdcfda9d99fd125523331e6645c8b5
KBear100 <kgamett@student.neumont.edu>
2024-01-09 14:19:27 -0700

    Dialog System

--- Assets/Scripts/DialogSystem.cs
+++ Assets/Scripts/DialogSystem.cs
@@ -8,18 +8,33 @@ public class DialogSystem : MonoBehaviour
 {
     [SerializeField] TMP_Text voiceLineText;
     [SerializeField] Image characterPicture;
+    [SerializeField] float speed;
 
-    private string[] voiceLines = {"Not many make it through the mirror maze. Still, I do not know your intentions. Many have tried hunting me to gain the power of wishing. None have succeeded. I am warning you: those with damaging dreams may not proceed further.",
+    private string[] eraVoiceLines = {"Not many make it through the mirror maze. Still, I do not know your intentions. Many have tried hunting me to gain the power of wishing. None have succeeded. I am warning you: those with damaging dreams may not proceed further.",
         "What is a damaging dream? The ones that take instead of give. The dreams that crush dreams. You've seen the result. The dwindling. A dream cannot sustain itself on the destruction of other dreams for long. It turns into an emptiness that spreads and consumes.",
         "I am not as trusting as Magus. If your intentions are pure, then I will tell you about wish magic. The power to make dreams reality. Perhaps, enough constructive dreaming will repair Dreamscapes and Happyton. I have prepared a trial for you. Another labyrinth Not a test of your resolve, but a test of your dreams."};
 
     void Start()
     {
-        
+        voiceLineText.text = "";
+        StartCoroutine(ShowDialog(eraVoiceLines[0]));
     }
 
     void Update()
     {
-        voiceLineText.text = voiceLines[0];
+
+    }
+
+    private IEnumerator ShowDialog(string dialog)
+    {
+        dialog.ToCharArray();
+        float letterSpeed = speed;
+
+        foreach (char letter in dialog)
+        {
+            letterSpeed = speed;
+            voiceLineText.text += letter;
+            yield return new WaitForSeconds(speed);
+        }
     }
 }
