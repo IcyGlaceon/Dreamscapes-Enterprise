6b584d1582c9db3157254fa70537c6d9ad31f8dc
leo5d1 <80261451+leo5d1@users.noreply.github.com>
2024-01-09 13:38:59 -0700

    collectables

--- Assets/Scripts/Collectables.cs
+++ Assets/Scripts/Collectables.cs
@@ -9,7 +9,6 @@ public class Collectables : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        Debug.Log("aaaaaa");
         if (collision.gameObject.tag == "Player")
         {
             gameManager.GainedCollectables++;
