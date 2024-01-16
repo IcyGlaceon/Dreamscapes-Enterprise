39bbe4b7132553276b340a60b98d489fcaa63792
KBear100 <kgamett@student.neumont.edu>
2024-01-16 14:45:30 -0700

    Collectables & Audio

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,9 +1,42 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    public int GainedCollectables = 0;
-    public int MaxCollectibles = 15;
+    [SerializeField] TMP_Text collectText;
+
+    public static GameManager Instance;
+    public static int currentCollectables = 0;
+    public static bool pinkCharacter = false;
+    public static bool blueCharacter = false;
+    public static bool greenCharacter = false;
+    public static bool redCharacter = false;
+    public static bool yellowCharacter = false;
+    public static bool cyanCharacter = false;
+    public static int GainedCollectables = 0;
+    public static int MaxCollectibles = 15;
+
+    private void Awake()
+    {
+        if (Instance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        collectText.text = GainedCollectables.ToString();
+    }
 }
--- Assets/Scripts/PolySecret.cs
+++ Assets/Scripts/PolySecret.cs
@@ -5,13 +5,11 @@ using static DialogSystem;
 
 public class PolySecret : MonoBehaviour
 {
-    [SerializeField] GameManager gameManager;
-
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.tag == "Player")
         {
-            if (gameManager.GainedCollectables == gameManager.MaxCollectibles)
+            if (GameManager.GainedCollectables == GameManager.MaxCollectibles)
             {
                 string line = "Congrats on getting all the dreams";
                 //StartCoroutine(DialogSystem.ShowDialog(line));
