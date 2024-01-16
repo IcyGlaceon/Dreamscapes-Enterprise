49b323c61ed1b73d5092826b85d3d805546a029d
EthanBarlevy <98473724+EthanBarlevy@users.noreply.github.com>
2024-01-16 16:18:36 -0700

    working on some new particles

--- Assets/Scripts/Collectables.cs
+++ Assets/Scripts/Collectables.cs
@@ -4,11 +4,17 @@ using UnityEngine;
 
 public class Collectables : MonoBehaviour
 {
+    [SerializeField] GameObject collectionParticle;
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.tag == "Player")
         {
             GameManager.GainedCollectables++;
+            if (collectionParticle)
+            { 
+                //Instantiate(collectionParticle, Collectables.transform.position, Collectables.transform.rotation);
+            }
             Destroy(gameObject);
         }
     }
