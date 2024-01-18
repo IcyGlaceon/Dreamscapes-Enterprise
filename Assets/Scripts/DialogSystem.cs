8460b1b194bac4d3d2a6dc570a4a862014862be0
KBear100 <kgamett@student.neumont.edu>
2024-01-18 14:44:10 -0700

    Face Moves & Disappears

--- Assets/Scripts/DialogSystem.cs
+++ Assets/Scripts/DialogSystem.cs
@@ -8,7 +8,8 @@ public class DialogSystem : MonoBehaviour
 {
     [Header("Dialog")]
     [SerializeField] TMP_Text voiceLineText;
-    [SerializeField] Image characterPicture;
+    [SerializeField] GameObject characterPicture;
+    [SerializeField] Sprite[] characterSprites;
     [SerializeField] float speed;
     [SerializeField] AudioSource audioSource;
     [SerializeField] AudioClip[] voiceLines;
@@ -18,11 +19,31 @@ public class DialogSystem : MonoBehaviour
     private string[] eraVoiceLine1 = { "Not many make it through the mirror maze. Still, I do not know your intentions.", "Many have tried hunting me to gain the power of wishing. None have succeeded.", "I am warning you: those with damaging dreams may not proceed further." };
     private string[] eraVoiceLine2 = { "What is a damaging dream? The ones that take instead of give.", "The dreams that crush dreams.", "You've seen the result. The dwindling.", "A dream cannot sustain itself on the destruction of other dreams for long. It turns into an emptiness that spreads and consumes." };
     private string[] eraVoiceLine3 = { "I am not as trusting as Magus. If your intentions are pure,", "then I will tell you about wish magic. The power to make dreams reality.", "Perhaps, enough constructive dreaming will repair Dreamscapes and Happyton.", "I have prepared a trial for you. Another labyrinth.", "Not a test of your resolve, but a test of your dreams." };
+    private int[] eraFaces1 = { 1, 1, 0 };
+    private int[] eraFaces2 = { 1, 1, 1, 1 };
+    private int[] eraFaces3 = { 1, 3, 3, 0, 0 };
 
+    //1
+    //1
+    //0
+    
+    
+    //1
+    //1
+    //1
+    //1
+    
+    
+    //1
+    //3
+    //3
+    //0
+    //0
     void Start()
     {
         voiceLineText.text = "";
-        StartCoroutine(ShowDialog(eraVoiceLine1, voiceLines[0]));
+
+        StartCoroutine(ShowDialog(eraVoiceLine1, eraFaces1, voiceLines[0]));
     }
 
     void Update()
@@ -30,14 +51,16 @@ public class DialogSystem : MonoBehaviour
         collectText.text = GameManager.GainedCollectables.ToString();
     }
 
-    public IEnumerator ShowDialog(string[] dialog, AudioClip voiceLine = null)
+    public IEnumerator ShowDialog(string[] dialog, int[] faceNumbers = null, AudioClip voiceLine = null)
     {
+        Image characterImage = characterPicture.GetComponent<Image>();
+        characterPicture.SetActive(true);
         audioSource.clip = voiceLine;
         audioSource.Play();
-        foreach (var voiceline in dialog)
+        for (int i = 0; i < dialog.Length; i++)
         {
-            voiceline.ToCharArray();
-            foreach (char letter in voiceline)
+            characterImage.sprite = characterSprites[faceNumbers[i]];
+            foreach (char letter in dialog[i])
             {
                 voiceLineText.text += letter;
                 yield return new WaitForSeconds(speed);
@@ -45,5 +68,6 @@ public class DialogSystem : MonoBehaviour
             yield return new WaitForSeconds(1.5f);
             voiceLineText.text = "";
         }
+        characterPicture.SetActive(false);
     }
 }
