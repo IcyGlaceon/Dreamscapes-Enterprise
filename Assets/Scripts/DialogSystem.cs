6848092fca93ffd2bfbd79d2a06f7272a6ff311c
KBear100 <kgamett@student.neumont.edu>
2024-01-19 15:30:22 -0700

    Transitions & Audio Splice

--- Assets/Scripts/DialogSystem.cs
+++ Assets/Scripts/DialogSystem.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class DialogSystem : MonoBehaviour
@@ -13,37 +14,26 @@ public class DialogSystem : MonoBehaviour
     [SerializeField] float speed;
     [SerializeField] AudioSource audioSource;
     [SerializeField] AudioClip[] voiceLines;
+    [Header("Stoping")]
+    [SerializeField] GameObject stopPlayer;
     [Header("Collectables")]
     [SerializeField] TMP_Text collectText;
 
     private string[] eraVoiceLine1 = { "Not many make it through the mirror maze. Still, I do not know your intentions.", "Many have tried hunting me to gain the power of wishing. None have succeeded.", "I am warning you: those with damaging dreams may not proceed further." };
     private string[] eraVoiceLine2 = { "What is a damaging dream? The ones that take instead of give.", "The dreams that crush dreams.", "You've seen the result. The dwindling.", "A dream cannot sustain itself on the destruction of other dreams for long. It turns into an emptiness that spreads and consumes." };
-    private string[] eraVoiceLine3 = { "I am not as trusting as Magus. If your intentions are pure,", "then I will tell you about wish magic. The power to make dreams reality.", "Perhaps, enough constructive dreaming will repair Dreamscapes and Happyton.", "I have prepared a trial for you. Another labyrinth.", "Not a test of your resolve, but a test of your dreams." };
+    private string[] eraVoiceLine3 = { "If your intentions are pure,", "then I will tell you about wish magic. The power to make dreams reality.", "Perhaps, enough constructive dreaming will repair Dreamscapes and Happyton.", "I have prepared a trial for you. Another labyrinth.", "Not a test of your resolve, but a test of your dreams." };
+    private string[] eraVoiceLineBeginning = { "I am not as trusting as Magus." };
     private int[] eraFaces1 = { 1, 1, 0 };
     private int[] eraFaces2 = { 1, 1, 1, 1 };
     private int[] eraFaces3 = { 1, 3, 3, 0, 0 };
 
-    //1
-    //1
-    //0
-    
-    
-    //1
-    //1
-    //1
-    //1
-    
-    
-    //1
-    //3
-    //3
-    //0
-    //0
+    private int currentLevel;
+
     void Start()
     {
         voiceLineText.text = "";
-
-        StartCoroutine(ShowDialog(eraVoiceLine1, eraFaces1, voiceLines[0]));
+        characterPicture.SetActive(false);
+        //StartCoroutine(ShowDialog(eraVoiceLine1, eraFaces1, voiceLines[0]));
     }
 
     void Update()
@@ -57,6 +47,7 @@ public class DialogSystem : MonoBehaviour
         characterPicture.SetActive(true);
         audioSource.clip = voiceLine;
         audioSource.Play();
+        currentLevel++;
         for (int i = 0; i < dialog.Length; i++)
         {
             characterImage.sprite = characterSprites[faceNumbers[i]];
@@ -69,5 +60,26 @@ public class DialogSystem : MonoBehaviour
             voiceLineText.text = "";
         }
         characterPicture.SetActive(false);
+        SceneManager.LoadSceneAsync("Level" +  currentLevel);
+    }
+
+    public void showLevelDialog(int level)
+    {
+        currentLevel = level;
+        switch(level)
+        {
+            case 0:
+                StartCoroutine(ShowDialog(eraVoiceLineBeginning, eraFaces1, voiceLines[3]));
+                break;
+            case 1:
+                StartCoroutine(ShowDialog(eraVoiceLine1, eraFaces1, voiceLines[0]));
+                break;
+            case 2:
+                StartCoroutine(ShowDialog(eraVoiceLine2, eraFaces2, voiceLines[1]));
+                break;
+            case 3:
+                StartCoroutine(ShowDialog(eraVoiceLine3, eraFaces3, voiceLines[2]));
+                break;
+        }
     }
 }
