29540245c9fb191cb1955f2b2983e2a8f73d3199
Carter Small <melaniesmall461@gmail.com>
2024-01-22 15:09:33 -0700

    I dont even fucking know

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -13,16 +13,20 @@ public class Movement : MonoBehaviour
     bool isGrounded = false;
     float storedPosition = 0;
 
+    [SerializeField] Transform ground;
+
     // Start is called before the first frame update
     void Start()
     {
         storedPosition = player.position.x;
-        //anim.SetBool("IsBlue", true);
+        anim.SetBool("IsBlue", true);
     }
 
     // Update is called once per frame
     void Update()
     {
+        RaycastHit2D hit = Physics2D.Raycast(ground.position, -Vector2.up);
+
         if (player.position.x - storedPosition < -0.3 || player.position.x - storedPosition > 0.3)
         {
             if (world != null)
@@ -34,19 +38,21 @@ public class Movement : MonoBehaviour
         }
         player.velocity = new Vector2(0, player.velocity.y);
 
-        if (player.velocity.y < 0)
-        {
-            isGrounded = false;
-        }
         if(player.velocity.y <= -1)
         {
+            isGrounded = false;
             anim.SetBool("IsRunning", false);
             anim.SetBool("IsFalling", true);
         }
-        // Debug.Log(player.velocity.y);
 
+        if(hit.collider.tag == "Platform")
+        {
+            anim.SetBool("IsRunning", true);
+            isGrounded = true;
+            anim.SetBool("IsFalling", false);
+        }
 
-        anim.SetBool("IsBlue", GameManager.blueCharacter);
+        //anim.SetBool("IsBlue", GameManager.blueCharacter);
         anim.SetBool("IsGreen", GameManager.greenCharacter);
         anim.SetBool("IsCyan", GameManager.cyanCharacter);
         anim.SetBool("IsRed", GameManager.redCharacter);
