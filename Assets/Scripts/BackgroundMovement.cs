318d1081bdf42fb8c1ac626b7b5a357035d96310
EthanBarlevy <98473724+EthanBarlevy@users.noreply.github.com>
2024-01-26 15:59:24 -0700

    fixed the bounce back "but like for real this time"

--- Assets/Scripts/BackgroundMovement.cs
+++ Assets/Scripts/BackgroundMovement.cs
@@ -1,5 +1,8 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
+using Unity.Collections.LowLevel.Unsafe;
 using UnityEngine;
 using UnityEngine.Events;
 using static UnityEngine.RuleTile.TilingRuleOutput;
@@ -14,6 +17,8 @@ public class BackgroundMovement : MonoBehaviour
     public bool bouncedBack = false;
 	public float bounceTime = 0;
 	Vector2 movementTrans;
+    private int count = 0;
+    private float prevSpeed = 0;
 
     void Start()
     {
@@ -26,33 +31,44 @@ public class BackgroundMovement : MonoBehaviour
     {
         movementTrans += Vector2.left * speed * Time.deltaTime;
         transform.position = movementTrans;
-        if (maxSpeed > speed)
+        if (maxSpeed > speed) // slow or bounced back
         {
-            if (slow)
+            if (slow) // if we are in a slow region
             {
-                if (slowSpeed > speed)
+                if (slowSpeed > speed) // go up to slow speed
                 {
                     speed += 0.05f;
                 }
+                if (slowSpeed < speed) 
+                { 
+                    speed = slowSpeed;
+                }
             }
-            else
+            else // otherwise go back to max speed
             {
                 speed += 0.05f;
                 bouncedBack = false;
             }
         }
-        else if (maxSpeed < speed)
+        else if (maxSpeed < speed) // make sure that speed is capped
         {
             speed = maxSpeed;
         }
         bounceTime -= Time.deltaTime;
+
+        if (prevSpeed > speed && speed > -3.5 && speed != slowSpeed) 
+        {
+            speed = prevSpeed;
+        }
+        prevSpeed = speed;
     }
 
     public void MoveBack()
     {
+        count++;
         if(speed == maxSpeed && bounceTime <= 0) 
         {
-            bouncedBack = true;
+			bouncedBack = true;
             transform.position = new Vector2(transform.position.x + 0.3f, 0);
             movementTrans = transform.position;
             speed = -4;
