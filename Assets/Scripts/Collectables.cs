d7b0995330c7c8603959a5157c8e33aa68a766e9
EthanBarlevy <98473724+EthanBarlevy@users.noreply.github.com>
2024-02-27 13:19:52 -0700

    removed file logging in collectable

--- Assets/Scripts/Collectables.cs
+++ Assets/Scripts/Collectables.cs
@@ -31,25 +31,3 @@ public class Collectables : MonoBehaviour
         }
     }
 }
-
-public class Data
-{
-    public Transform ParticleTransform;
-    public Vector3 ParticlePosition;
-    public Quaternion ParticleRotation;
-    public Vector3 ParticleScale;
-    public Vector3 PlayerPosition;
-    public Quaternion PlayerRotation;
-    public Vector3 PlayerScale;
-    
-    public Data(Transform transform, Vector3 position, Quaternion rotation, Vector3 scale, Vector3 playerPosition, Quaternion playerRotation, Vector3 playerScale)
-	{
-		this.ParticleTransform = transform;
-		this.ParticlePosition = position;
-		this.ParticleRotation = rotation;
-		this.ParticleScale = scale;
-		PlayerPosition = playerPosition;
-		PlayerRotation = playerRotation;
-		PlayerScale = playerScale;
-	}
-}
