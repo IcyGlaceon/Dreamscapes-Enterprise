171f50e3a6337ef2f5fc20f952b9afd0b2bd8ab8
leo5d1 <80261451+leo5d1@users.noreply.github.com>
2024-02-27 13:08:25 -0700

    Collectable comments

--- Assets/Scripts/Collectables.cs
+++ Assets/Scripts/Collectables.cs
@@ -6,19 +6,27 @@ using UnityEngine;
 
 public class Collectables : MonoBehaviour
 {
+    // collectionParticle = the particle used for when the player grabs the collectable
+    // Player = player object in world
     [SerializeField] GameObject collectionParticle;
     [SerializeField] GameObject Player;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        // Checks to see if the Collider belongs to Player
         if (collision.gameObject.tag == "Player")
         {
+            // Plays collection sounds
             GameManager.collectSound.Play();
+            // Increments the total gained collectables in the game manager
             GameManager.GainedCollectables++;
+            // Checks to see if there is a valid particle for collection
             if (collectionParticle)
             {
+                // creates the particle
                 Instantiate(collectionParticle, Player.transform.position, collectionParticle.transform.rotation);
 			}
+            // destroys the collectable
             Destroy(gameObject);
         }
     }
